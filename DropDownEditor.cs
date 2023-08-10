54b3af183d5cbef64a6033c1b26df7c708d6f04b
AGitHub <101417758+ArinGujarati@users.noreply.github.com>
2023-08-10 20:53:24 +0530

    Add files via upload

---
+++ DropDownEditor.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+public class DropDownEditor : MonoBehaviour
+{
+    [Header("Basic")]
+    public int health;
+    public int defense;
+    public float movementSpeed;
+
+    [Header("Combat")]
+    public int attack;
+    public float attackRange;
+    public float attackSpeed;
+
+    [Header("Magic")]
+    public int magicResistance;
+    public bool hasMagic;
+    public int mana;
+    public enum MagicElementType { Fire, Water, Earth, Air };
+    public MagicElementType magicType;
+    public int magicDamage;
+}
